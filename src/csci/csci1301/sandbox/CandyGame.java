7fab561b8acdf7acc7aec9cddf3890dca97ed420
u1tralord <poston.jacob@gmail.com>
2016-01-13 08:19:06 -0500

    Initial Commit

---
+++ src/csci/csci1301/sandbox/CandyGame.java
@@ -0,0 +1,17 @@
+package csci.csci1301.sandbox;
+
+import java.util.Scanner;
+
+/**
+ * Created by Jacob on 1/13/2016.
+ */
+public class CandyGame {
+    int playerTake = 0;
+    int aiTake = 0;
+    int basketCount = 21;
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+    }
+}
