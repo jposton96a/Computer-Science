8dd26f44f197746dfa0ce37e9ef064d364840cb4
Jacob Poston <poston.jacob@gmail.com>
2016-01-30 22:38:32 -0500

    Added mutation ability to Genome.java

---
+++ src/csci/CSCI_1301/sandbox/neural_network/core/Genome.java
@@ -0,0 +1,45 @@
+package csci.CSCI_1301.sandbox.neural_network.core;
+
+/**
+ * Created by jposton on 1/29/16.
+ */
+public class Genome {
+
+    float[] genes;
+    float min, max;
+
+    public Genome(float min, float max){
+        this(min, max, 10);
+    }
+
+    public Genome(float min, float max, int size){
+        genes = new float[size];
+
+        for(int i = 0; i < size; i++){
+            genes[i] = min + (float)(Math.random() * (max-min));
+        }
+    }
+
+    public void mutate(float mutationRate){
+        for(int i = 0; i < genes.length; i++){
+            if(Math.random() < mutationRate){
+                genes[i] = min + (float)(Math.random() * (max-min));
+            }
+        }
+    }
+
+    public Genome getOffspring(Genome g1){
+        if(this.getSize() == g1.getSize()) {
+            float[] newGeneSeq = new float[g1.getSize()];
+        }
+        return null;
+    }
+
+    public float[] getSequence(){
+        return this.genes;
+    }
+
+    public int getSize(){
+        return genes.length;
+    }
+}
